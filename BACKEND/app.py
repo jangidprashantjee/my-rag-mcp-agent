fe2b1d9176128e2dc8abf7d38c7e7e1292c9e2c7
jangidprashantjee <jangidprashantjee@gmail.com>
2025-08-12 23:24:21 +0530

    Add cors origin for frontend

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS # only for development
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://sampledemo-n8e7.onrender.com"])
 app.register_blueprint(llm_routes)
 
 if __name__ == "__main__":
