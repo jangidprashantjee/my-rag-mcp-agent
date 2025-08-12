97d3793cbbd3e44a4022bd1fca816cadcbbb7508
jangidprashantjee <jangidprashantjee@gmail.com>
2025-08-13 00:12:18 +0530

    port issue fixec

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from routes import llm_routes
 from flask_cors import CORS # only for development
-
+import os
 
 app = Flask(__name__)
 CORS(app, origins=["https://sampledemo-n8e7.onrender.com"])
 app.register_blueprint(llm_routes)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
